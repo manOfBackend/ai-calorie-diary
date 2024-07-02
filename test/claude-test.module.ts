8322954f7df9c4b50b00d9a5619d64fa01730f5d
hyunbell <man.of.backend@gmail.com>
2024-07-03 02:10:10 +0900

    test: claude e2e

--- test/claude-test.module.ts
+++ test/claude-test.module.ts
@@ -4,17 +4,8 @@ import { ClaudeService } from '../src/claude/application/service/claude.service'
 import { ClaudeApiPort } from '../src/claude/application/port/out/claude-api.port';
 
 class MockClaudeApiAdapter implements ClaudeApiPort {
-  async streamResponse(
-    prompt: string,
-  ): Promise<AsyncIterable<{ content: string }>> {
-    const mockResponses = [
-      'Hello',
-      ', how',
-      ' can I',
-      ' help you',
-      ' today?',
-      prompt,
-    ];
+  async streamResponse(): Promise<AsyncIterable<{ content: string }>> {
+    const mockResponses = ['Hello', ', how', ' can I', ' help you', ' today?'];
     return (async function* () {
       for (const response of mockResponses) {
         yield { content: response };
@@ -23,8 +14,8 @@ class MockClaudeApiAdapter implements ClaudeApiPort {
     })();
   }
 
-  async singleResponse(prompt: string): Promise<{ content: string }> {
-    return { content: `This is a mock response from Claude API. :: ${prompt}` };
+  async singleResponse(): Promise<{ content: string }> {
+    return { content: `This is a mock response from Claude API.` };
   }
 }
 
