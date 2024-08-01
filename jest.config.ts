767f9de88be64b1bbeef868fee7421ade68e091c
hyunbell <man.of.backend@gmail.com>
2024-08-01 14:00:52 +0900

    feat: Enable resolveJsonModule in tsconfig.json
    This commit adds the "resolveJsonModule" option to the tsconfig.json file, enabling the resolution of JSON modules. This is necessary for importing JSON files as modules in the application.

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from '@jest/types';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+};
+
+export default config;
